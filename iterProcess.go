e498436f120899eb234e6a20a3cb74173eec8968
hadoop <hadoop@hadoop6.(none)>
2015-03-17 17:16:17 +0800

    add deriate time

--- iterProcess.go
+++ iterProcess.go
@@ -35,6 +35,7 @@ func main(){
 	rset := mapset.NewSet()
 	startTime:= time.Now()
 	cnt:= 0
+	callMap := make(map[int64][]int64) 
 	for _, m :=range getData(){
 		str_time := time.Unix(int64(m.Timestamp), 0).Format("2006-01-02 15:04:05")
 		fmt.Println(m,str_time)
@@ -42,6 +43,30 @@ func main(){
 		rset.Add(m.Mac)
 		//zoom.DeleteById("ProcessData",m.GetId())
 		cnt+=1
+		
+		//find value;
+		_,exists:=callMap[m.Mac]
+		if exists{
+				tmpp:= callMap[m.Mac]
+				nz:=len(tmpp)
+				if(m.Timestamp-tmpp[nz-2] - tmpp[nz-1] >300){
+					callMap[m.Mac] = append(callMap[m.Mac],m.Timestamp)
+					callMap[m.Mac] = append(callMap[m.Mac],0)
+				}else{
+					tmpp[nz-1]= m.Timestamp-callMap[m.Mac][nz-2]
+				}
+		}else{
+			callMap[m.Mac] = append(callMap[m.Mac],int64(m.Timestamp))
+			callMap[m.Mac] = append(callMap[m.Mac],0)
+		}
+	}
+	for i,m := range callMap{
+			fmt.Println(i,m)
+			for i,v:= range m{
+				if(i%2==0){
+					fmt.Println( time.Unix(int64(v), 0).Format("2006-01-02 15:04:05"))
+				}
+			}
 	}
 	fmt.Println("count unique mac...", rset.Cardinality(),"  cnt.. ", cnt)
 	fmt.Println(time.Now().Sub(startTime))
