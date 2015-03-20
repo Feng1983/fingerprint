c8d86c451f704808a662e2c1985c8d407f24b5b5
hadoop <hadoop@hadoop6.(none)>
2015-03-20 18:35:53 +0800

    add newfile

---
+++ PostgresqlStorage.go
@@ -0,0 +1,191 @@
+mackage main
+
+import (
+	"database/sql"
+	"fmt"
+	//_ "github.com/bmizerany/pq"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+	"github.com/albrow/zoom"
+	"log"
+	"time"
+	"errors"
+	"knn2"
+)
+var (
+	au = []int{5,6,8,}
+	ch = []int{9,12,13,}
+	ErrNoPqSQLConn     = errors.New("can't get a mysql db")
+	ErrNoData		   = errors.New("NO data found.")
+	ErrExist		   = errors.New("data exists ,return.")
+)
+
+
+type SampleData struct {
+	Id       int32
+	Ts       int64 `gorm:"column:timestamp"`
+	Inframac int16 `gorm:"column:infra_mac"`
+	Devmac   int64 `gorm:"column:device_mac"`
+	Rssi     int16
+	Freq     int16 `gorm:"column:frequency"`
+}
+
+type FinMatchData struct{
+	StoreId	int32 `gorm:"column:store_id"`
+	UserId	int32 `gorm:"column:user_id"`
+	Ts		int64 
+	Mac		int64
+	X		float64
+	Y		float64 
+}
+type PostgresqlStorage{
+	DB	*gorm.DB	
+}
+//init sqlStorage
+func NewPostgresqlStorage() *PostgresqlStorage{
+	db, err := gorm.Open("postgres", "host=123.57.254.158 user=postgres password=111111 dbname=ailink_wifi sslmode=disable")
+    if err != nil {
+        fmt.Println("connection error...", err)
+    }
+	RedisInit()
+    return &PostgresqlStorage{DB:&db}
+}
+
+func (b SampleData) TableName() string {
+	return "samples"
+}
+
+
+
+func closeORdb(db *gorm.DB) error {
+	err := db.Close()
+	return err
+}
+
+func doIter(db *gorm.DB) {
+	var users, users2 []*SampleData
+	db = db.Table("samples")
+	//db.AutoMigrate(&SampleData{})
+	db.Limit(10).Find(&users)
+	for i, d := range users {
+		fmt.Println(i, d)
+		//fmt.Println(d.Ts)
+		fmt.Println(time.Unix(d.Ts, 0).Format("2006-01-02 15:04:05"))
+	}
+	dd := getTime("2015-03-10 12:44:33")
+	db.Where("timestamp >= ? ", dd).Limit(20).Find(&users2)
+	for _, d := range users2 {
+		fmt.Println(d)
+		fmt.Println(time.Unix(d.Ts, 0).Format("2006-01-02 15:04:05"))
+	}
+	var psize int
+	dd2 := getTime("2015-03-12 12:44:33")
+	db.Where("timestamp >= ? and timestamp <?", dd, dd2).Count(&psize)
+	fmt.Println(psize)
+
+}
+func (pqsDb *PostgresqlStorage) FectchItem(params interface{},dd1 ,dd2 int64) error{
+	var users [] *SampleData
+	db := pqsDb.DB
+	if db==nil{
+		return ErrNoPqSQLConn 
+	}
+	db = db.Table("samples")
+	//rows,err:= db.Raw("select * from samples where infra_mac in (?)",param)
+	//defer rows.Close()
+	db.Where("infra_mac in (?)",params).Where("timestamp >= ? and timestamp <?",dd1,dd2).Limit(200).Find(&users)
+	for _ , d :=range users{
+		fmt.Println(d,time.Unix(d.Ts, 0).Format("2006-01-02 15:04:05"))
+	}
+	var psize int
+	db.Where("infra_mac in (?)",params).Where("timestamp >= ? and timestamp <?",dd1,dd2).Count(&psize)
+	fmt.Println(psize)
+	return nil
+}
+
+func (pqsDb  *PostgresqlStorage) LoadFingers(loc int) error{
+	var findata []*knn2.FingerOri
+	var id in
+	db := pqsDb.DB
+	if db==nil{
+		return ErrNoPqSQLConn
+	}
+	res,_ := zoom.NewQuery("Finger2").Filter("Id= ",loc).Count()
+	if res !=0{
+	 return ErrNoData
+	}
+	if loc ==1{
+		db=db.Table("au_finger_data")
+		id=1
+	}else{
+		db=db.Table("cn_finger_data")
+		id=0
+	}
+	db.Find(&findata)
+
+	for _, d :=range findata{
+		//fmt.Println(d)
+		fs:=make(map[int64]float64)
+		fs[int64(d.Ap1)]= d.Freq1
+		fs[int64(d.Ap2)]= d.Freq2
+		fs[int64(d.Ap3)]= d.Freq3
+		
+		fin:=&knn2.Finger2{Id:id,Label:d.Id,X:d.X,Y:d.Y,Feature:fs}
+		fmt.Println(fin)
+		zoom.save(fin)
+	}
+	return nil
+}
+
+func saveDate(db *gorm.DB)error{
+	return nil
+}
+
+func (pqsDb  *PostgresqlStorage) SaveFingerData(datas[]* knn2.ProcessData, userid, storeid int) error{
+	db := pqsDb.DB
+	if db==nil{
+		return ErrNoPqSQLConn
+	}
+	if len(datas)==0{
+		return  ErrNoData
+	}
+	for _, d:=range datas{
+		ds:= FinMatchData{
+			StoreId:storeid,
+			UserId:userid,
+			Ts:d.Timestamp,
+			Mac:d.Mac,
+			X: d.X,
+			Y: d.Y,		
+		}	
+		db.Create(&ds)
+	}
+	
+	return nil
+}
+
+func RedisInit(){
+    conf:= &zoom.Configuration{
+        Address:"localhost:59999",
+        Network:"tcp",
+    }
+    zoom.Init(conf)
+    zoom.Register(&knn2.Finger{})
+    zoom.Register(&knn2.ProcessData{})
+	zoom.Register(&knn2.Finger2{})
+}
+
+
+func main() {
+	var dbs *sql.DB
+	dbs = openDB()
+	testdb(dbs)
+	closeDB(dbs)
+	db := ormInit()
+	//doIter(db)
+	dd1:=getTime("2015-03-17 00:00:00")
+	dd2:=getTime("2015-03-18 00:00:00")
+	fectchItem(db,[]int{5,6,8},dd1,dd2)
+	loadFingers(1,db)
+	closeORdb(db)
+}
---
+++ processData.go
@@ -0,0 +1,196 @@
+package main
+
+import(
+	"github.com/albrow/zoom"
+	"github.com/deckarep/golang-set"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+	"os"
+	"bufio"
+	"knn2"
+)
+
+type Rssiample struct{
+	Id int
+	Ts int		`zoom:"index"`
+	Imac int64	`zoom:"index"`
+    Dmac int64	`zoom:"index"`
+	Rss  int
+	Frq  int
+	zoom.DefaultData
+} 
+
+type RdataSet struct{
+    Samples []* knn2.MapBaseSample
+    max_lable  int
+}
+
+type Info struct{
+    x map[int64]int
+}
+
+type RIter []*Rssiample
+
+func Init(){
+	conf:= &zoom.Configuration{
+        Address:"localhost:59999",
+        Network:"tcp",
+    }
+	zoom.Init(conf)
+	//zoom.Register(&Person{})
+	zoom.Register(&Rssiample{})
+	zoom.Register(&knn2.Finger{})
+	zoom.Register(&knn2.Finger2{})
+	zoom.Register(&knn2.ProcessData{})
+}
+
+func getSamples(imac int64) []*Rssiample{
+	var sp []*Rssiample
+	if(imac==int64(-1)){
+		if err:= zoom.NewQuery("Rssiample").Order("Ts").Scan(&sp);err!=nil{
+            panic(err)
+        }
+	}else{
+		if err:= zoom.NewQuery("Rssiample").Order("Ts").Filter("Imac =",imac).Scan(&sp);err!=nil{
+			panic(err)
+		}
+	}
+	return sp
+}
+
+func getFingers(id int)[]* knn2.Finger2{
+	var rs[]* knn2.Finger2
+	if err:=zoom.NewQuery("Finger2").Filter("Id= ",id).Order("Label").Scan(&rs);err!=nil{
+		panic(err)
+	}
+	return rs
+}
+
+func (i RIter)Iterator() func()( *Rssiample,bool){
+	index:=0
+	return func()(val *Rssiample, ok bool){
+			if index>=len(i){
+				return
+			}
+			val, ok = i[index],true
+			index++
+			return
+	}
+}
+
+
+func etldata(samples []*Rssiample)[]* knn2.MapBaseSample{
+	size:= len(samples)
+	if size<=0{
+		return nil
+	}
+	start,end:= samples[0],samples[size-1]
+	var samples1= RIter(samples)
+	it:=samples1.Iterator()
+	var ret []* knn2.MapBaseSample
+	for i:=start.Ts+2;i<=end.Ts+1;i+=2{
+		mapr:=make(map[int64] *Info)
+		for{
+			val,ok := it()
+			if !ok{
+				break
+			}
+			if val.Ts >i {
+				break
+			}
+			if v, exist := mapr[val.Dmac];!exist{
+				tmp:=&Info{}
+				rv:=make(map[int64]int)
+				rv[val.Imac]=val.Rss
+				tmp.x=rv
+				mapr[int64(val.Dmac)]=tmp
+				//fmt.Println(val.Rss)
+			}else{
+				(v.x)[val.Imac] =val.Rss
+				mapr[val.Dmac]=v
+			}
+		}
+		for s,t :=range mapr{
+			ret = append(ret,&knn2.MapBaseSample{Features:t.x,Timestamp:int64(i),Mac:int64(s)})
+		}
+	}
+	return ret
+}
+
+func getSampleFromdb(id int, dd string)[]*Rssiample{
+	var ret []*Rssiample
+	
+
+}
+func procETLdata(id int, dd string) []*knn2.MapBaseSample{
+	var proc_data []* knn2.MapBaseSample
+	sp := getSampleFromdb(id, dd)
+	pp := etldata(sp)
+	cnt,rcnt:=0,0
+	for _ , m:=range pp{
+		if len(m.Features)>=2{
+            //str_time := time.Unix(int64(m.Timestamp), 0).Format("2006-01-02 15:04:05")
+            //fmt.Println(m,str_time)
+            rcnt+=1
+            if len(m.Features)==2{
+                for _,v :=range m.Features{
+                    if v>-70 {
+                        rset.Add(m.Mac)
+                        //fmt.Println(k,"...",v)
+                        proc_data=append(proc_data,m)
+                        break
+                    }
+                }
+            }else if len(m.Features)==3{
+                rset.Add(m.Mac)
+                proc_data=append(proc_data, m)
+            }else{
+                rset.Add(m.Mac)
+            }
+            //rset.Add(m.Mac)
+        }
+        for _,v:=range m.Features{
+            if v>-60 {
+                rset.Add(m.Mac)
+            }
+        }
+        //rset.Add(m.Mac)
+        cnt+=1
+	}
+	return proc_data
+}
+
+func processFingerDataById(id int,proc_data []*knn2.MapBaseSample) []*knn2.ProcessData{
+	params:=make(map[string]string)
+    params["k"]="5"
+    obj:=&knn2.KNN{}
+    obj.Init(params)
+    fingdata:= getFingers(id)//getFingers
+    obj.Train2(fingdata)
+	var ret []*knn2.ProcessData
+
+    for _,m :=range proc_data{
+        if len(m.Features)>=2{
+            str_time := time.Unix(int64(m.Timestamp), 0).Format("2006-01-02 15:04:05")
+            fmt.Println(m,str_time)
+            mx,my:= obj.Predict2(m)
+            fmt.Println(mx,"y... ",my)
+            prdata:= &knn2.ProcessData{Timestamp:m.Timestamp, Mac:m.Mac,X:mx, Y:my}
+            fmt.Println(prdata)
+            //zoom.Save(prdata)
+			append(ret,prdata)
+        }
+    }
+	return ret
+}
+
+func main(){
+	Init()
+	starttime:=time.Now()
+
+	
+	fmt.Println(time.Now().Sub(starttime))
+	defer zoom.Close()
+}
